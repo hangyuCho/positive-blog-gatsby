762f48880c312525618750a3622b5532f1d36343
hangyu cho <chg3376@gmail.com>
2020-12-12 18:07:35 +0900

    fix : error fix when build by netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [{
