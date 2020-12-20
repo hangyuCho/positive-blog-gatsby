8a00191e1be39e9793551fab7b484f4244305069
hangyu cho <chg3376@gmail.com>
2020-12-20 10:23:54 +0900

    add : iframe page

---
+++ src/pages/gantt.js
@@ -0,0 +1,16 @@
+import React from "react"
+const Gantt = () => {
+  return (
+    <div>
+      <iframe 
+      title="shard"
+      style={{
+        width: `85em`,
+        height: `40em`
+      }}
+      src={"https://docs.google.com/spreadsheets/d/1sexeNF2p4k3yF9c3Y5qjxxHNDgfvL61kbdFda8aoZV8/edit?usp=sharing"}></iframe>
+    </div>
+  )
+}
+
+export default Gantt
